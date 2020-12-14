829e5e76b7b65ddbd943e6b3a6468a4be86d9561
Bryan Dela Cruz <bryanmdlx@gmail.com>
2020-12-15 04:08:55 +0800

    started adding logs
    - todo: filter which modules are allowed to log

--- src/game_pad_service.rs
+++ src/game_pad_service.rs
@@ -13,21 +13,41 @@ impl GamePadService for GamePadServiceImpl {
         &self,
         request: Request<Streaming<Input>>,
     ) -> Result<Response<Output>, Status> {
+        let remote_addr = request.remote_addr();
+
+        log::info!(
+            "Client ({:?}) requested to instantiate game pad.",
+            remote_addr
+        );
+
         let mut stream = request.into_inner();
 
         let mut game_pad = GamePad::create().map_err(|_e| {
             Status::internal("An error occurred while trying to create game pad device.")
         })?;
 
+        log::info!("Instantiated game pad for client ({:?}).", remote_addr);
+
         while let Some(input) = stream.next().await {
             let data = input?.control.ok_or(Status::invalid_argument(
                 "The `control` argument is missing!",
             ))?;
 
+            log::info!(
+                "Received game pad input data ({:?}) from client ({:?}).",
+                data,
+                remote_addr
+            );
+
             game_pad.control(data).map_err(|_e| {
                 Status::internal("An error occurred while trying to control the game pad device.")
             })?;
         }
+
+        log::info!(
+            "Destroying game pad that was made for client ({:?}).",
+            remote_addr
+        );
 
         Ok(Response::new(Output {}))
     }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use simple_logger::SimpleLogger;
 use tonic::transport::Server;
 
 mod game_pad;
@@ -6,6 +7,10 @@ mod proto;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    SimpleLogger::new()
+        .with_level(log::LevelFilter::Debug)
+        .init()?;
+
     let addr = "[::1]:50000".parse()?;
 
     Server::builder()
